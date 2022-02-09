087e02e0d21b2c5963c967784972ac08795f7eb8
mdrondado <mdrondado@hotmail.com>
2022-02-09 09:36:41 -0300

    improvement in curriculum and index pages

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Site Pessoal
+
+      IMPLEMENTATION LOG   
+
+DOING:  
+- add images description for accessibility;
+- testing for bugs verification;
+
+TO DO:
+- show career technical keywords in curriculum page;
+- change color and format of webpage logo;
+- add software logos in curriculum page;
+- add page about the author;
+- implement multi-language EN-ES-PT in pages;
+
+DONE:   
+02/08/2022 - add formation and companies logos;  
+02/08/2022 - add authors contacts and social network links;
+02/07/2022 - simplify index.html page by removing section headers;
--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
-console.log('Olá mundo, JavaScript!');
+console.log("Olá mundo, JavaScript!");
 
-function alteraBotao () {
-  document.getElementById('curriculo').textContent = 'Curriculum';
+function alteraBotao() {
+  document.getElementById("curriculo").textContent = "Curriculum";
 }
 
-
-function alterarPagina(paginaAberta){
-  if (paginaAberta == 'curriculo') {
-    document.getElementById('tituloprofissi').textContent = 'Logistics, Transportation and Technology Professional';
+function alterarPagina(paginaAberta) {
+  if (paginaAberta == "curriculo") {
+    document.getElementById("tituloprofissi").textContent =
+      "Logistics, Transportation and Technology Professional";
   }
 }
 
-const nome = 'Gama Academy' ; 
-console . log ( nome . padStart ( 13 ) ) ; 
-console . log ( nome . padStart ( 2 ) ) ;
+const nome = "Constante";
+console.log(nome.padStart(13));
+console.log(nome.padStart(2));
